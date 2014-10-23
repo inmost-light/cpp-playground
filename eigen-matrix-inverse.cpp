c628e04bd2b5cd56b13f23550aae0772d0e9d462
inmost-light <inmost-light@users.noreply.github.com>
2014-10-24 00:02:09 +0300

    Add spaces.

--- eigen-matrix-inverse.cpp
+++ eigen-matrix-inverse.cpp
@@ -5,8 +5,8 @@ using namespace std;
 auto main() -> int {
   auto m = Eigen::Matrix3f{};
   m << 1,0,0,
-    0,5,0,
-    0,0,9;
+       0,5,0,
+       0,0,9;
 
   cout << m.inverse() << endl
        << endl
